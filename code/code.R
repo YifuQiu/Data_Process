8af09dbb0dda2e9f5adc19b00d7f9726cb50e7da
yifu <y.qiu@alumnos.upm.es>
2019-12-18 16:58:43 +0100

    update 4 related work

--- code/code.R
+++ code/code.R
@@ -10,3 +10,17 @@ Sys.setenv(SPOTIFY_CLIENT_ID = client_id)
 Sys.setenv(SPOTIFY_CLIENT_SECRET = client_secret)
 access_token <- get_spotify_access_token()
 
+data <- get_artist_audio_features('the beatles', include_groups = c('album', 'single'))
+artists <- c('queen','eminem')
+for (artist in artists){
+  audio_features <- get_artist_audio_features(artist, include_groups = c('album', 'single'))
+  data <- rbind(data, audio_features)
+}
+
+data <- data %>% 
+  select(danceability, energy, key, loudness, speechiness, acousticness, instrumentalness, liveness, valence, tempo, track_id, mode_name)
+
+
+xd = get_artist_audio_features(artist = 'the beatles', include_groups = c("album", "single"), return_closest_artist = TRUE, dedupe_albums = TRUE,authorization = get_spotify_access_token())
+
+View(xd)
