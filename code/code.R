fae5021e902ce7c5d1c8324afb77c705341c2ae3
Alejandro-Cobo <alejandro.cobo.cabornero@alumnos.upm.es>
2019-12-18 16:39:59 +0100

    (UPD) Update the code to add the popularity of the tracks to the dataset

--- code/code.R
+++ code/code.R
@@ -1,6 +1,6 @@
 # Libraries
 library(spotifyr)
-library(tidyverse)
+library(dplyr)
 library(ggplot2)
 
 # Authentication
@@ -8,15 +8,24 @@ client_id <- paste(readLines("../keys/client_id.txt"), collapse=" ")
 client_secret <- paste(readLines("../keys/client_secret.txt"), collapse=" ")
 Sys.setenv(SPOTIFY_CLIENT_ID = client_id)
 Sys.setenv(SPOTIFY_CLIENT_SECRET = client_secret)
-access_token <- get_spotify_access_token()
 
-data <- get_artist_audio_features('the beatles', include_groups = c('album', 'single'))
-artists <- c('queen','eminem')
+artists <- c('the beatles')
+pop = c()
+i = 1
 for (artist in artists){
-  audio_features <- get_artist_audio_features(artist, include_groups = c('album', 'single'))
-  data <- rbind(data, audio_features)
+  audio_features <- get_artist_audio_features(artist, include_groups = c('album'))
+  if (i == 1) {
+    data <- audio_features
+  } else {
+    data <- rbind(data, audio_features)
+  }
+  for (id in audio_features$track_id){
+    track <- get_track(id)
+    pop[i] <- track$popularity
+    i <- i + 1
+  }
 }
 
 data <- data %>% 
-  select(danceability, energy, key, loudness, speechiness, acousticness, instrumentalness, liveness, valence, tempo, track_id, mode_name)
-
+  select(danceability, energy, key, loudness, speechiness, acousticness, instrumentalness, liveness, valence, tempo, track_id, mode_name, -track_id) %>% 
+  mutate(popularity = pop)
